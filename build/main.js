78058896d6380881707cb3a91a2eb8c17f2c2ce9
magictimon <retzlaff.timon@gmail.com>
2021-01-06 21:22:39 +0100

    added new command

--- build/main.js
+++ build/main.js
@@ -30,12 +30,20 @@ var MyApp = /** @class */ (function () {
             quit: function (user, params, command) {
                 user.getAppContentSession(AppViewMode.Overlay).remove();
             },
+            givePoints: function (user, params, command) {
+                if (user.isChannelModerator()) {
+                    var paramsArray = params.split(" ");
+                    _this.addUpgradePoints(parseInt(paramsArray[0]), _this.getUserByNick(paramsArray[1]));
+                    user.sendPrivateMessage("Set points of " + paramsArray[1] + " to " + _this.getUserByNick(paramsArray[1]).getPersistence().getNumber("upgradePoints"));
+                }
+            }
         };
     }
     MyApp.prototype.onUserJoined = function (user) {
         user.sendPrivateMessage("Gib /restart ein um das Spiel neu zu starten und gib /quit ein um das Spiel zu beenden");
         var file = new HTMLFile('index.html', {});
         user.sendAppContent(AppContent.overlayContent(file, 330, 570));
+        user.getPersistence().addNumber("upgradePoints", 0);
     };
     /*    public onPublicMessage(msg:PublicMessage) {
             if (msg.getAuthor().isChannelModerator()) {
@@ -48,6 +56,19 @@ var MyApp = /** @class */ (function () {
             return false;
         }
         return true;
+    };
+    MyApp.prototype.addUpgradePoints = function (toAdd, user) {
+        var persistence = user.getPersistence();
+        persistence.setNumber("upgradePoints", persistence.getNumber("upgradePoints", 0) + toAdd);
+    };
+    MyApp.prototype.subUpgradePoints = function (toSub, user) {
+        var persistence = user.getPersistence();
+        persistence.setNumber("upgradePoints", persistence.getNumber("upgradePoints", 0) - toSub);
+    };
+    MyApp.prototype.getUserByNick = function (nick) {
+        var userAcces = KnuddelsServer.getUserAccess();
+        var user = userAcces.getUserById(userAcces.getUserId(nick));
+        return user;
     };
     return MyApp;
 }());
--- src/server/main/Main.ts
+++ src/server/main/Main.ts
@@ -3,6 +3,7 @@ class MyApp implements App {
         user.sendPrivateMessage("Gib /restart ein um das Spiel neu zu starten und gib /quit ein um das Spiel zu beenden");
         let file = new HTMLFile('index.html', {});
         user.sendAppContent(AppContent.overlayContent(file, 330, 570));
+        user.getPersistence().addNumber("upgradePoints", 0);
     }
     /*    public onPublicMessage(msg:PublicMessage) {
             if (msg.getAuthor().isChannelModerator()) {
@@ -15,6 +16,19 @@ class MyApp implements App {
             return false;
         }
         return true;
+    }
+    private addUpgradePoints(toAdd: number, user: User) {
+        const persistence = user.getPersistence();
+        persistence.setNumber("upgradePoints", persistence.getNumber("upgradePoints", 0) + toAdd);
+    }
+    private subUpgradePoints(toSub: number, user: User) {
+        const persistence = user.getPersistence();
+        persistence.setNumber("upgradePoints", persistence.getNumber("upgradePoints", 0) - toSub);
+    }
+    private getUserByNick(nick: string): User {
+        const userAcces = KnuddelsServer.getUserAccess();
+        const user = userAcces.getUserById(userAcces.getUserId(nick));
+        return user;
     }
     public chatCommands = {
         kill: (user: User, params: string, command: string) => {
@@ -44,6 +58,13 @@ class MyApp implements App {
         quit: (user: User, params: string, command: string) => {
             user.getAppContentSession(AppViewMode.Overlay).remove();
         },
+        givePoints: (user: User, params: string, command: string) => {
+            if (user.isChannelModerator()) {
+                const paramsArray = params.split(" ");
+                this.addUpgradePoints(parseInt(paramsArray[0]), this.getUserByNick(paramsArray[1]));
+                user.sendPrivateMessage("Set points of " + paramsArray[1] + " to " + this.getUserByNick(paramsArray[1]).getPersistence().getNumber("upgradePoints"));
+            }
+        }
     }
 }
 
