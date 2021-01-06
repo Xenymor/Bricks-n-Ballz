c0a3b5a5ecc878786dd20fa8d2cd2b3cedcc45be
magictimon <retzlaff.timon@gmail.com>
2021-01-06 20:00:28 +0100

    added commands

--- build/main.js
+++ build/main.js
@@ -1,10 +1,49 @@
 "use strict";
 var MyApp = /** @class */ (function () {
     function MyApp() {
+        var _this = this;
+        this.chatCommands = {
+            kill: function (user, params, command) {
+                if (user.isChannelModerator()) {
+                    var userAcces = KnuddelsServer.getUserAccess();
+                    var victim = userAcces.getUserById(userAcces.getUserId(params));
+                    if (victim.isAppDeveloper()) {
+                        return;
+                    }
+                    var persistence = victim.getPersistence();
+                    persistence.setNumber("muted", 1);
+                    user.sendPrivateMessage(victim.getNick() + " was muted");
+                }
+            },
+            pardon: function (user, params, command) {
+                if (user.isChannelModerator()) {
+                    var userAcces = KnuddelsServer.getUserAccess();
+                    var pardoned = userAcces.getUserById(userAcces.getUserId(params));
+                    var persistence = pardoned.getPersistence();
+                    persistence.deleteNumber("muted");
+                }
+            },
+            restart: function (user, params, command) {
+                _this.onUserJoined(user);
+            },
+        };
     }
     MyApp.prototype.onUserJoined = function (user) {
+        user.sendPrivateMessage("Gib /restart ein um das Spiel neu zu starten");
         var file = new HTMLFile('index.html', {});
         user.sendAppContent(AppContent.overlayContent(file, 330, 570));
+    };
+    /*    public onPublicMessage(msg:PublicMessage) {
+            if (msg.getAuthor().isChannelModerator()) {
+                if (msg.getRawText().contains("/kick"))
+            }
+        }
+    */
+    MyApp.prototype.mayShowPublicMessage = function (msg) {
+        if (msg.getAuthor().getPersistence().getNumber("muted", 0) == 1) {
+            return false;
+        }
+        return true;
     };
     return MyApp;
 }());
--- src/server/main/Main.ts
+++ src/server/main/Main.ts
@@ -1,7 +1,45 @@
 class MyApp implements App {
     public onUserJoined(user: User) {
+        user.sendPrivateMessage("Gib /restart ein um das Spiel neu zu starten");
         let file = new HTMLFile('index.html', {});
-        user.sendAppContent(AppContent.overlayContent(file, 330, 570));        
+        user.sendAppContent(AppContent.overlayContent(file, 330, 570));
+    }
+    /*    public onPublicMessage(msg:PublicMessage) {
+            if (msg.getAuthor().isChannelModerator()) {
+                if (msg.getRawText().contains("/kick"))
+            }
+        }
+    */
+    public mayShowPublicMessage(msg: PublicMessage): boolean {
+        if (msg.getAuthor().getPersistence().getNumber("muted", 0) == 1) {
+            return false;
+        }
+        return true;
+    }
+    public chatCommands = {
+        kill: (user: User, params: string, command: string) => {
+            if (user.isChannelModerator()) {
+                const userAcces = KnuddelsServer.getUserAccess();
+                const victim = userAcces.getUserById(userAcces.getUserId(params));
+                if (victim.isAppDeveloper()) {
+                    return;
+                }
+                const persistence = victim.getPersistence();
+                persistence.setNumber("muted", 1);
+                user.sendPrivateMessage(victim.getNick() + " was muted");
+            }
+        },
+        pardon: (user: User, params: string, command: string) => {
+            if (user.isChannelModerator()) {
+                const userAcces = KnuddelsServer.getUserAccess();
+                const pardoned = userAcces.getUserById(userAcces.getUserId(params));
+                const persistence = pardoned.getPersistence();
+                persistence.deleteNumber("muted");
+            }
+        },
+        restart: (user: User, params: string, command: string) => {
+            this.onUserJoined(user);
+        },
     }
 }
 
